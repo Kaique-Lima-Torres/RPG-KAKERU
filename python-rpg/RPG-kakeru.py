5a88d3b7d5f951ca28b2c3b81d404a07dfb7779e
KAIQUE LIMA TORRES <102607065+Kaique-Lima-Torres@users.noreply.github.com>
2022-11-15 04:35:30 -0300

    Add files via upload

---
+++ python-rpg/RPG-kakeru.py
@@ -0,0 +1,84 @@
+import os
+
+run = True
+menu = True
+play = False
+rules = False
+
+HP = 50
+ATK = 3
+
+def clear():
+    os.system("cls")
+
+def draw():
+    print("Xx------------------------xX")
+
+def save():
+    list = [
+        name,
+        str(HP),
+        str(ATK)
+    ]
+
+    f = open("load.txt","w")
+
+    for item in list:
+        f.write(item + "\n")
+    f.close()
+
+
+while run:
+    while menu:
+        clear()
+        draw()
+        print('1, NEW GAME')
+        print('2, LOAD GAME')
+        print('3, RULES')
+        print('4, QUIT GAME')
+        draw()
+
+        if rules:
+            print("I'm the creator of this game and these are the rule.")
+            rules = False
+            choice = ''
+            input('> ')
+        else:
+            choice = input('# ')
+
+        if choice == '1':
+            clear()
+            name = input("# what's your name, hero: ")
+            menu = False
+            play = True
+        elif choice == '2':
+            f = open("load.txt","r")
+            load_list = f.readlines()
+            name = load_list[0][:-1]
+            HP = load_list[1][:-1]
+            ATK = load_list[2][:-1]
+            clear()
+            print("welcome back, " + name + "!")
+            input("> ")
+            menu = False
+            play = True
+        elif choice == '3':
+            rules = True
+        elif choice == '4':
+            quit()
+
+    while play:
+        save() #auto save
+
+
+        clear()
+        draw()
+        print("0 - SAVE AND QUIT")
+        draw()
+        
+        dest = input("# ")
+        
+        if dest == '0':    
+            play = False
+            menu = True
+            save()
